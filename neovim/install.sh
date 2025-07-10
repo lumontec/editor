7ced06f1be3be404695935ec78a16be1ee15286e
lmontech <luca.montechiesi@siemens.com>
2025-07-10 16:30:04 -0700

    Update neovim/install: support multi arch through .local-$arch dir

--- neovim/install.sh
+++ neovim/install.sh
@@ -18,7 +18,7 @@ esac
 echo "Detected architecture: $arch"
 echo "Installing Neovim from $nvim_dir"
 
-nvim_install_path="$HOME/.local"
+nvim_install_path="$HOME/.local-$arch"
 
 echo "Installing Neovim under $nvim_install_path"
 mkdir -p "$nvim_install_path/bin" "$nvim_install_path/lib" "$nvim_install_path/share"
@@ -30,14 +30,17 @@ cp -r "./$nvim_dir/share/"* "$nvim_install_path/share"
 nvim_bin="$nvim_install_path/bin"
 nvim_runtime="$nvim_install_path/share/nvim/runtime"
 
-# Add alias to .bashrc if not already present
-if ! grep -q "alias nvim='VIMRUNTIME=$nvim_runtime nvim'" ~/.bashrc; then
-    echo "alias nvim='VIMRUNTIME=$nvim_runtime nvim'" >> ~/.bashrc
-fi
+if ! grep -q "^nvim()" ~/.bashrc; then
+    cat << 'EOF' >> ~/.bashrc
 
-# Add Neovim binary path to .bashrc if not already present
-if ! grep -q "export PATH=$nvim_bin:\$PATH" ~/.bashrc; then
-    echo "export PATH=$nvim_bin:\$PATH" >> ~/.bashrc
+# Neovim function with architecture-specific paths
+nvim() {
+    XDG_DATA_HOME=/home/lmontech/.local-$(uname -m)/share \
+    XDG_STATE_HOME=/home/lmontech/.local-$(uname -m)/state \
+    VIMRUNTIME=/home/lmontech/.local-$(uname -m)/share/nvim/runtime \
+    /home/lmontech/.local-$(uname -m)/bin/nvim "$@"
+}
+EOF
 fi
 
 echo "Copying over initialization files"
@@ -46,3 +49,9 @@ cp -rf ./kickstart.nvim "$HOME/.config/nvim"
 
 echo "Neovim installation complete. Please restart your shell or run 'source ~/.bashrc' to apply changes."
 
+### info for clangd install on arm ###
+# It will likely not work with mason !!, use this hack instead
+# sudo apt install clangd-16
+# ln -s /usr/bin/clangd-16 ~/.local/share/nvim/mason/bin/clangd
+# mkdir ~/.local/share/nvim/mason/packages/clangd# 
+#
